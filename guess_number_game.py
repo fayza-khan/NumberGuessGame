aa18047c0e213ff314c0f9884b6269f26a30c547
Fayza Khan <70770111+fayza-khan@users.noreply.github.com>
2020-10-10 20:34:53 +0530

    Update guess_number_game.py

--- guess_number_game.py
+++ guess_number_game.py
@@ -1,20 +1,5 @@
 # Guessing Number Game
 
-"""
-The Challenge:
-
-Write a program that picks a random integer from 1 to 100, and has players guess the number. The rules are:
-
-If a player's guess is less than 1 or greater than 100, say "OUT OF BOUNDS"
-On a player's first turn, if their guess is
-within 10 of the number, return "WARM!"
-further than 10 away from the number, return "COLD!"
-On all subsequent turns, if a guess is
-closer to the number than the previous guess return "WARMER!"
-farther from the number than the previous guess, return "COLDER!"
-When the player's guess equals the number, tell them they've guessed correctly and how many guesses it took!
-"""
-
 from random import randint
 x = randint(1, 100)
 print("WELCOME TO GUESS ME!")
@@ -24,7 +9,6 @@ print("If your guess is within 10 of my number, I'll tell you you're WARM")
 print("If your guess is farther than your most recent guess, I'll say you're getting COLDER")
 print("If your guess is closer than your most recent guess, I'll say you're getting WARMER")
 print("LET'S PLAY!")
-y = 1
 guesses = [0]
 while True:
     num = int(input("Please enter a number here:"))
